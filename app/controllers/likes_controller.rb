71a319715151fc201097d3fb8fb5317b7fb6f289
Matheus Siqueira <matheusbatista.dev@gmail.com>
2024-04-14 14:18:18 -0300

    first commit

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,19 +3,16 @@ class LikesController < ApplicationController
     before_action :find_comment 
     def create
         @post = Post.find(params[:post_id])
-
         @post.likes.create(likeable_id: @post.id, user_id: current_user.id, likeable_type: Post.name)
     end
 
     def destroy
-        # binding.pry
         @post = Post.find_by(id: params[:post_id]) 
         @post.likes.where(likeable_id: @post.id, user_id: current_user.id, likeable_type: Post.name)
             .delete_all
     end
 
     def create_comments_like
-        # binding.pry
         @comment = Comment.find_by(id: params[:comment_id])
         @comment.likes
             .create(likeable_id: @comment.id, user_id: current_user.id, likeable_type: Comment.name)
